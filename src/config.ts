74c834e410b17d34e41ae22401e89d3f94562d6c
Eduardo Ortega <ortegon000@hotmail.com>
2024-07-22 16:17:48 -0600

    first commit

---
+++ src/config.ts
@@ -0,0 +1,153 @@
+import Facebook from "./icons/socials/facebook.astro";
+import Instagram from "./icons/socials/instagram.astro";
+import X from "./icons/socials/x.astro";
+import Linkedin from "./icons/socials/linkedin.astro";
+
+export const siteConfig = {
+    siteName: 'My Awesome Site',
+    siteLogo: '/img/logo.png',
+    siteProfession: 'Pediatra Alergologo',
+    siteAddress: '47274 Ferry Loop, Jewellville, ME 16393',
+    siteShortDescription: 'Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit​​, morbo vel maleficia? De Apocalypsi undead dictum mauris.',
+    sitePhone: '+525512333997',
+    siteEmail: 'ortegon000@gmail.com',
+    siteHeroImage: '/img/me.jpg',
+    siteImage: '/img/me-2.jpg',
+    siteMap: 'https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3760.2004772984383!2d-99.17165012410932!3d19.533004781767925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTnCsDMxJzU4LjgiTiA5OcKwMTAnMDguNyJX!5e0!3m2!1ses-419!2smx!4v1721683087765!5m2!1ses-419!2smx',
+    shcedule: [
+        {
+            day: 'Lunes',
+            start: '8:00am',
+            end: '6:00pm'
+        },
+        {
+            day: 'Martes',
+            start: '8:00am',
+            end: '6:00pm'
+        },
+        {
+            day: 'Miercoles',
+            start: '8:00am',
+            end: '6:00pm'
+        },
+        {
+            day: 'Jueves',
+            start: '8:00am',
+            end: '6:00pm'
+        },
+        {
+            day: 'Viernes',
+            start: '8:00am',
+            end: '6:00pm'
+        },
+    ],
+    socials: [
+        {
+            name: 'facebook',
+            url: 'https://facebook.com',
+            icon: Facebook
+        },
+        {
+            name: 'instagaram',
+            url: 'https://instagram.com',
+            icon: Instagram
+        },
+        {
+            name: 'x',
+            url: 'https://x.com',
+            icon: X
+        },
+        {
+            name: 'linkedin',
+            url: 'https://linkedin.com',
+            icon: Linkedin
+        }
+    ],
+    services: [
+        {
+            name: 'Servicio 1',
+            description: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.',
+            image: 'https://picsum.photos/400/600'
+        },
+        {
+            name: 'Servicio 2',
+            description: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.',
+            image: 'https://picsum.photos/400/600'
+        },
+        {
+            name: 'Servicio 3',
+            description: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.',
+            image: 'https://picsum.photos/400/600'
+        },
+        {
+            name: 'Servicio 4',
+            description: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.',
+            image: 'https://picsum.photos/400/600'
+        },
+        {
+            name: 'Servicio 5',
+            description: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.',
+            image: 'https://picsum.photos/400/600'
+        },
+        {
+            name: 'Servicio 6',
+            description: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.',
+            image: 'https://picsum.photos/400/600'
+        },
+    ],
+    gallery: [
+        {
+            url: 'https://picsum.photos/400/600',
+            alt: 'alt text'
+        },
+        {
+            url: 'https://picsum.photos/400/600',
+            alt: 'alt text'
+        },
+        {
+            url: 'https://picsum.photos/400/600',
+            alt: 'alt text'
+        },
+        {
+            url: 'https://picsum.photos/400/600',
+            alt: 'alt text'
+        },
+        {
+            url: 'https://picsum.photos/400/600',
+            alt: 'alt text'
+        },
+        {
+            url: 'https://picsum.photos/400/600',
+            alt: 'alt text'
+        },
+        {
+            url: 'https://picsum.photos/400/600',
+            alt: 'alt text'
+        },
+        {
+            url: 'https://picsum.photos/400/600',
+            alt: 'alt text'
+        },
+        {
+            url: 'https://picsum.photos/400/600',
+            alt: 'alt text'
+        },
+    ],
+    opinions: [
+        {
+            'opinion': 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.',
+            'name': 'John Doe',
+            'avatar': 'https://i.pravatar.cc/300',
+        },
+        {
+            'opinion': 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.',
+            'name': 'John Doe',
+            'avatar': 'https://i.pravatar.cc/300',
+        },
+        {
+            'opinion': 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.',
+            'name': 'John Doe',
+            'avatar': 'https://i.pravatar.cc/300',
+        },
+    ]
+};
