ea9a2d06d14a92c72cec7e3ade6ff35b251fe35a
Eduardo Ortega <ortegon000@hotmail.com>
2025-05-09 19:46:07 -0600

    feat: implement initial website layout with header, hero section and footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import alpinejs from "@astrojs/alpinejs";
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs()],
+  integrations: [alpinejs(), icon({
+    
+  })],
   vite: {
     plugins: [tailwindcss()],
   },
